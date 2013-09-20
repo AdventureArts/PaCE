e94687717182e5eb74acd6aca5c825db91c4e7d8
UltCombo <ult_combo@hotmail.com>
2013-09-19 22:02:10 -0300

    testing

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ define('pace',
 	[        'jquery', 'crafty', 'canvas', 'support'],
 	function(    $,     Crafty,   canvas,   support) {
 	'use strict';
-	console.log('pace loaded');
+	console.log('pace dev loaded');
 
 
 	return {
